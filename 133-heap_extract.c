e16f289c3d8ea716846c8dcbe9fc9ea6c57a4c1c
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2024-01-04 19:11:28 +0000

    heap extract

---
+++ 133-heap_extract.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+* heap_extract - extracts the root node of a max binary heap
+* @root: double pointer
+* Return: if fails return 0
+*/
+int heap_extract(heap_t **root)
+{
+	if (!root || !*root)
+		return (0);
+	return (1);
+}
