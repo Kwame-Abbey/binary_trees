3dcffdd93b92d74336432c6801f3a2e45e70254f
Abbey Kwame Morgan <constancekwameabbey@gmail.com>
2024-01-04 18:46:20 +0000

    AVL Tree

---
+++ 121-avl_insert.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * avl_insert - inserts a value in an AVL Tree
+ * @tree: double pointer to the root node
+ * @value: value to store in the node to be inserted
+ * Return: pointer to the created node or NULL on failure
+ */
+avl_t *avl_insert(avl_t **tree, int value)
+{
+	(void) tree;
+	(void) value;
+	return (NULL);
+}
